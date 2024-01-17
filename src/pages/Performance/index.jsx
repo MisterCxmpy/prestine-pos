a7841d46173a6edb2dd37d158fff6a3d738a0ea0
MisterCxmpy <cosgunhussein@gmail.com>
2024-01-17 21:37:08 +0000

    price stuff

--- src/pages/Performance/index.jsx
+++ src/pages/Performance/index.jsx
@@ -13,7 +13,7 @@ export default function Performance() {
         <h1>Performance Stats</h1>
         <ul className={styles['performance-list']}>
           <PerformanceItem heading={"Taken In*"} value={todaysPerformance[0]?.takenIn} desc={"The amount of items brought into the shop today."} />
-          <PerformanceItem heading={"Earnings*"} value={"£" + (Math.abs(todaysPerformance[0]?.earnings)).toFixed(2)} desc={"The amount of money earned today."} />
+          <PerformanceItem heading={"Earnings*"} value={"£" + ((Math.abs(todaysPerformance[0]?.earnings * 0.25))).toFixed(2)} desc={"The amount of money earned today."} />
         </ul>
       </div>
       <PerformanceTable performanceData={performance} />
